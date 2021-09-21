5e44c8efb31b13d698e7fcb547825a312cfe44bb
Gary Magneson <77851427+GaryMagneson@users.noreply.github.com>
2021-09-21 10:38:29 -0500

    latest coursework

--- Angular/course-project/src/app/recipes/recipe-list/recipe-list.component.ts
+++ Angular/course-project/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,3 +1,4 @@
+import { Recipe } from './../recipe.model';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -6,7 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-
+  recipes: Recipe[] = [
+    new Recipe('A Test Recipe', 'This is a Test',
+                    'https://d1e3z2jco40k3v.cloudfront.net/-/media/mccormick-us/recipes/mccormick/q/2000/quick_and_easy_french_toast_new_2000x1125.jpg')
+  ];
   constructor() { }
 
   ngOnInit(): void {
