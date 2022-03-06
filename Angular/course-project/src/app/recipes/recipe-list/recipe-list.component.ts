c4aa985230506ff3f7209f51447b5b8a1ee10017
Gary Magneson <77851427+GaryMagneson@users.noreply.github.com>
2022-03-06 10:49:04 -0600

    lesson updates

--- Angular/course-project/src/app/recipes/recipe-list/recipe-list.component.ts
+++ Angular/course-project/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -9,7 +9,10 @@ import { Component, OnInit } from '@angular/core';
 export class RecipeListComponent implements OnInit {
   recipes: Recipe[] = [
     new Recipe('A Test Recipe', 'This is a Test',
-                    'https://d1e3z2jco40k3v.cloudfront.net/-/media/mccormick-us/recipes/mccormick/q/2000/quick_and_easy_french_toast_new_2000x1125.jpg')
+                    'https://d1e3z2jco40k3v.cloudfront.net/-/media/mccormick-us/recipes/mccormick/q/2000/quick_and_easy_french_toast_new_2000x1125.jpg'),
+    new Recipe('A Test Recipe', 'This is a Test too',
+    'https://d1e3z2jco40k3v.cloudfront.net/-/media/mccormick-us/recipes/mccormick/q/2000/quick_and_easy_french_toast_new_2000x1125.jpg')
+
   ];
   constructor() { }
 
